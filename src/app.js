ae675c73f6a526d56664b10b0ead19857071a693
bourama1 <b.matej@email.cz>
2026-04-03 16:00:07 +0200

    Set trust proxy for Express app

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 // Middleware
 const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 const corsOptions = {
